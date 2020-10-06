41cfa66202e3391334b88cc4c3dfd18fdbefc6db
rahmd0012 <rahmd0012@wrdsb.ca>
2020-10-06 16:18:03 +0000

    Program showed number of stars for what a number user entered

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * a program that writes a line of * each time for a user entered number
+ * @author Daniel Rahmani
  */
 public class Main {
 
@@ -9,7 +10,17 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // create scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // ask the user for a number between 1 and 10
+    System.out.println("Please enter a number between 1 and 10");
+    int n = input.nextInt();
+
+    // create a for loop
+    for(int count = 1; count <= n; count++){
+      System.out.print("*");
+    }
     
   }
 }
